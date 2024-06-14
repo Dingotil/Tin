794b1f3afd597d5a09046d918908a953f6785ff7
Dingotil <164393187+Dingotil@users.noreply.github.com>
2024-06-14 13:06:42 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 function navigateTo(url) {
     window.location.href = url;
 }
-// Función para mostrar la animación de carga
-function showLoader() {
-  document.querySelector('.loader-container').style.display = 'flex';
-}
-
-// Función para ocultar la animación de carga
-function hideLoader() {
-  document.querySelector('.loader-container').style.display = 'none';
-  document.querySelector('.content').classList.add('loaded');
-}
-
-// Mostrar la animación de carga al inicio
-showLoader();
-
-// Ocultar la animación de carga cuando la página esté cargada
+// Agregamos un evento para cuando la página esté cargada
 window.addEventListener('load', function() {
-  hideLoader();
+  // Ocultamos la animación de carga
+  document.querySelector('.loading-container').classList.add('loaded');
 });
