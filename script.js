88b4da2b09ba29ba6c0f1539c3b2ce35ce578991
Dingotil <164393187+Dingotil@users.noreply.github.com>
2024-06-14 13:20:01 -0500

    Actualizar script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function navigateTo(url) {
-    window.location.href = url;
-}
-// Agregamos un evento para cuando la página esté cargada
-window.addEventListener('load', function() {
-  // Ocultamos la animación de carga
-  document.querySelector('.loading-container').classList.add('loaded');
-});
